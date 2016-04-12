4091a0d378876e4fa0cae59c2278462f18cdb31f
Dung Nguyen <dungntnew@gmail.com>
2016-04-13 00:42:08 +0900

    init project

---
+++ src/main/java/com/ajisai/boom/AjisaiApp.java
@@ -0,0 +1,12 @@
+package com.ajisai.boom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjisaiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjisaiApp.class, args);
+	}
+}
